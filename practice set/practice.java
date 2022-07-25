287e4c6585f478e9745d69525954e2a83b23ee1e
Neelamgusain08 <85832729+Neelamgusain08@users.noreply.github.com>
2022-07-25 23:19:58 +0530

    Create practice.java

---
+++ practice set/practice.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class practice {
+    public static void main(String[] args) {
+        //question 1
+
+//     System.out.println("what is your name");
+//     Scanner sc=new Scanner(System.in);
+//     String name=sc.next();
+//     System.out.print("hello "+name );
+// sc.close();
+
+//question 2 ;convert kilometer into miles
+// System.out.println("Enter the value in kilometers");
+
+//         System.out.println("Enter the value in kilometers");
+
+        // Scanner sc = new Scanner(System.in);
+
+        // double value = sc.nextInt();
+
+        // double miles = (value/1.609);
+
+        // System.out.println("The value in miles is  "  +  miles  +  "  miles");
+
+//         double miles = (value/1.609);
+
+//         System.out.println("The value in miles is  "  +  miles  +  "  miles");
+
+
+
+//question 3
+//whether the input no. is integer or not
+System.out.println("enter the integer");
+
+Scanner sc = new Scanner(System.in);
+System.out.println(sc.hasNextInt());
+
+
+sc.close();
+
+        
+    }
+    
+}
